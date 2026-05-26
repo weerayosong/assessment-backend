20a6c7af9f879f987776c9ea8db21d92d519086d
weerayosong <weerayosong@outlook.com>
2026-05-26 11:38:38 +0700

    add mock data, fix some typo

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # JSD12 Week13 | Assessment-Backend
 
-##API Server 5 endpoints with `products`
+## API Server, 5 endpoints with `products`
 
 ![cool-image](./cool.png)
---
+++ src/models/product.model.js
@@ -0,0 +1,27 @@
+export let products = [
+    { id: String(Date.now()), name: "Lightsaber", price: 1500, quantity: 2 },
+    {
+        id: String(Date.now() + 1),
+        name: "Blaster Pistol",
+        price: 800,
+        quantity: 5,
+    },
+    {
+        id: String(Date.now() + 2),
+        name: "Thermal Detonator",
+        price: 300,
+        quantity: 10,
+    },
+    {
+        id: String(Date.now() + 3),
+        name: "Mandalorian Armor",
+        price: 5000,
+        quantity: 1,
+    },
+    {
+        id: String(Date.now() + 4),
+        name: "Jedi Holocron",
+        price: 10000,
+        quantity: 1,
+    },
+];
